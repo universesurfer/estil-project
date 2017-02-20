3afd612f49584c37bdf84ba496640384a76a567b
Jeff <jzinger100@gmail.com>
2017-02-20 21:35:01 +0100

    user pictures and stylist portfolio created with uploads working

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var Stylist = require('../models/stylist.js');
+const Picture = require('../models/picture.js');
+const ensureLogin = require("connect-ensure-login");
+var multer  = require('multer');
+var upload = multer({ dest: './public/uploads/' });
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {user: req.user});
 });
 
 
@@ -27,5 +33,52 @@ router.post('/search/results',(req, res, next) => {
     }
   })
 })
+
+router.get('/profile/pictures', ensureLogin.ensureLoggedIn("/login"), function(req,res) {
+	Picture.find({"user" : req.user.username},(err, pictures) => {
+		console.log(pictures);
+		res.render('private/profile-pictures', {pictures})
+	})
+})
+
+router.post('/profile/pictures/upload', upload.single('file'), function(req, res){
+
+  pic = new Picture({
+    name: req.body.name,
+    pic_path: `/uploads/${req.file.filename}`,
+    pic_name: req.file.originalname,
+		user: req.user.username
+  });
+
+	console.log(req.user.username);
+
+  pic.save((err) => {
+      res.redirect('/profile/pictures');
+  });
+});
+
+router.get('/stylist/profile/portfolio', ensureLogin.ensureLoggedIn("/stylist/login"), function(req,res) {
+	Picture.find({"user" : req.user.username},(err, pictures) => {
+		console.log(pictures);
+		res.render('private/stylist-portfolio', {pictures})
+	})
+})
+
+router.post('/stylist/profile/portfolio/upload', upload.single('file'), function(req, res){
+
+  pic = new Picture({
+    name: req.body.name,
+    pic_path: `/uploads/${req.file.filename}`,
+    pic_name: req.file.originalname,
+		user: req.user.username
+  });
+
+	console.log(req.user.username);
+
+  pic.save((err) => {
+      res.redirect('/stylist/profile/portfolio');
+  });
+});
+
 
 module.exports = router;
