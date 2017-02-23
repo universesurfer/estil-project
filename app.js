7bec12efad44e8c863935525983d385c95236b60
Jeff <jzinger100@gmail.com>
2017-02-23 13:34:33 +0100

    committing app.js before git pull

--- app.js
+++ app.js
@@ -34,8 +34,6 @@ app.set('layout', 'layouts/main-layout');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
-
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
