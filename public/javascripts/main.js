71e765b512eddd094163fcccbeebb89e1d58944f
Jeff <jzinger100@gmail.com>
2017-02-24 09:56:21 +0100

    password strength meter added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,26 @@
-$('#main-nav li').click(function(e) {
-	// console.log($(this));
-	// $('#main-nav li.active').removeClass('active');
-  // var $this = $(this);
-  // if (!$this.hasClass('active')) {
-  //   $this.addClass('active');
-  // }
-  // e.preventDefault();
+var strength = {
+  0: "Worst",
+  1: "Bad",
+  2: "Weak",
+  3: "Good",
+  4: "Strong"
+}
+
+var password = document.getElementById('input4');
+var meter = document.getElementById('password-strength-meter');
+var text = document.getElementById('password-strength-text');
+
+password.addEventListener('input', function() {
+  var val = password.value;
+  var result = zxcvbn(val);
+
+  // Update the password strength meter
+  meter.value = result.score;
+
+  // Update the text indicator
+  if (val !== "") {
+    text.innerHTML = "Strength: " + strength[result.score];
+  } else {
+    text.innerHTML = "";
+  }
 });
