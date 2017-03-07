f2d48dc78052bf1b56ef1a12fcc4eb0b12387d6a
Jeff <jzinger100@gmail.com>
2017-03-07 17:21:44 +0100

    commit before moving branch

--- angular/src/app/app.routing.ts
+++ angular/src/app/app.routing.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
 
 import { SessionService } from './session.service';
 
 export const routes: Routes = [
     { path: 'signup', component: SignupComponent},
     { path: 'login', component: LoginComponent},
-    { path: 'profile', component: ProfileComponent, canActivate: [SessionService]}
+    { path: 'profile', component: ProfileComponent, canActivate: [SessionService]},
+    { path: 'search', component: SearchComponent}
 ];
