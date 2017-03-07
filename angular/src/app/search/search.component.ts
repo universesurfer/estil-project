c9ea3c1fb84dcd144268a212ff503d2dc20c0daa
Jeff <jzinger100@gmail.com>
2017-03-07 19:32:28 +0100

    autocomplete working, need to chain functions

--- angular/src/app/search/search.component.ts
+++ angular/src/app/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 declare var google: any;
+declare var map: any;
+
 
 @Component({
   selector: 'app-search',
@@ -12,22 +14,91 @@ export class SearchComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    var mapOptions = {
-            center: new google.maps.LatLng(51.508742, -0.120850),
-            zoom: 5,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    console.log(document.getElementById("map"));
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.showMapWithMyLocation);
+    } else {
+      alert('Oops, no geolocation support');
+    }
+
   }
 
-  getMyLocation() {
-    // if (navigator.geolocation) {
-    //   navigator.geolocation.getCurrentPosition(showMapWithMyLocation);
-    // } else {
-    //   alert('Oops, no geolocation support');
-    // }
+  showMapWithMyLocation(position) {
+    //The latitude and longitude values obtained from HTML 5 API.
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+  	var myPosition = {
+  		lat: latitude,
+  		lng: longitude
+  	}
+
+  	var map = new google.maps.Map(
+  		document.getElementById('map'),
+  		{
+  		 zoom: 15,
+  		 center: myPosition
+  		}
+  	);
+
+    console.log(this);
+
+    // this.runAutocomplete();
+
+    var mapInput = document.getElementById('pac-input');
+  	var newArea = new google.maps.places.Autocomplete(mapInput);
+  	var type = "Area Changed";
+  	newArea.bindTo('bounds', map);
+  	map.controls[google.maps.ControlPosition.TOP_LEFT].push(mapInput);
+
+  	var infowindow = new google.maps.InfoWindow();
+
+  	google.maps.event.addListener(newArea, 'place_changed', function() {
+  		infowindow.close();
+  		var place = newArea.getPlace();
+  		if (!place.geometry) {
+  			return;
+  		}
+
+  		if (place.geometry.viewport) {
+  			map.fitBounds(place.geometry.viewport);
+  		} else {
+  			map.setCenter(place.geometry.location);
+  			map.setZoom(17);
+  		}
+
+  	});
+
   }
+
+  // runAutocomplete() {
+  //
+  // 	//takes input and performs autocomplete
+  // 	var mapInput = document.getElementById('pac-input');
+  // 	var newArea = new google.maps.places.Autocomplete(mapInput);
+  // 	var type = "Area Changed";
+  // 	newArea.bindTo('bounds', map);
+  // 	map.controls[google.maps.ControlPosition.TOP_LEFT].push(mapInput);
+  //
+  // 	var infowindow = new google.maps.InfoWindow();
+  //
+  // 	google.maps.event.addListener(newArea, 'place_changed', function() {
+  // 		infowindow.close();
+  // 		var place = newArea.getPlace();
+  // 		if (!place.geometry) {
+  // 			return;
+  // 		}
+  //
+  // 		if (place.geometry.viewport) {
+  // 			map.fitBounds(place.geometry.viewport);
+  // 		} else {
+  // 			map.setCenter(place.geometry.location);
+  // 			map.setZoom(17);
+  // 		}
+  //
+  // 	});
+  //
+  // }
+
 
 }
