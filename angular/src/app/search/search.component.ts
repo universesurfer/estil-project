d0662ba897c7645d0e0dc0ec5309fe720ee70e46
Jeff <jzinger100@gmail.com>
2017-03-07 17:50:04 +0100

    map on page

--- angular/src/app/search/search.component.ts
+++ angular/src/app/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var google: any;
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -10,6 +12,22 @@ export class SearchComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var mapOptions = {
+            center: new google.maps.LatLng(51.508742, -0.120850),
+            zoom: 5,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    console.log(document.getElementById("map"));
+  }
+
+  getMyLocation() {
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(showMapWithMyLocation);
+    // } else {
+    //   alert('Oops, no geolocation support');
+    // }
   }
 
 }
