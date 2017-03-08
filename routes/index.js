cfe04ca5dd478a787330f87bdac4534048f40678
Jeff <jzinger100@gmail.com>
2017-03-08 12:12:20 +0100

    added search get request api

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Stylist = require('../models/stylist.js');
+
+router.get("/api/search", (req, res)=> {
+	Stylist.find({}, (err, allStylists) => {
+		mapInfo = {};
+		allStylists.forEach(function(stylist, index){
+			mapInfo["prop" + index] = stylist;
+		});
+		res.json(mapInfo);
+	});
+
+});
 
 module.exports = router;
