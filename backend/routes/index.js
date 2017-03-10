27a85c399f76692f5aefae026f7d900df608be0f
Jeff <jzinger100@gmail.com>
2017-03-10 20:34:22 +0100

    cant get distances using mongoose, sending to front end

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,10 +5,6 @@ const User           = require("../models/user");
 var Stylist = require('../models/stylist.js');
 
 router.post("/api/search", (req, res)=> {
-	console.log(req.body);
-	var distances = [];
-	var mapInfo = {};
-
 	Stylist.geoNear( req.body,
 		{ spherical : true,
 		 	maxDistance: 50,
@@ -17,57 +13,10 @@ router.post("/api/search", (req, res)=> {
 	    if (err) {
 	        console.log(err);
 	    } else {
-	        console.log(results);
+				res.json(results);
 	    }
-	}).then(function(err, results) {
-			console.log("results", results);
-
-			// process.exit();
-		});
-
-	Stylist.find({}, (err, allStylists) => {
-		mapInfo = {};
-		allStylists.forEach(function(stylist, index){
-			mapInfo["prop" + index] = stylist;
-		});
-		res.json(mapInfo);
-	});
+	})
 });
-
-
-// results.forEach(function(stylist,index){
-// 	// distances.push(stylist.dis);
-// 	console.log("stylist",stylist);
-// 	mapInfo["prop" + index] = stylist.obj;
-// 	mapInfo["prop" + index].distanceFromLocation = stylist.dis;
-//
-// });
-//
-// // mapInfo.prop4.test = "test";
-//
-// console.log("mapInfo",mapInfo);
-// res.json(mapInfo);
-
-
-		// for (var stylist in mapInfo) {
-		// 	var i = Object.keys(mapInfo).indexOf(stylist)
-		// 	mapInfo[stylist].distanceFromLocation = distances[i];
-		// 	console.log(distances[i]);
-		// }
-		// console.log(distances);
-		//
-		// Stylist.find({}, (err, allStylists) => {
-		// 	var mapInfo = {};
-		// 	allStylists.forEach(function(stylist, index){
-		// 		mapInfo["prop" + index] = stylist;
-		// 		mapInfo["prop" + index].distanceFromLocation = distances[index];
-		// 		console.log(mapInfo);
-		// 	});
-		// 	res.json(mapInfo);
-		// });
-
-
-
 
 
 router.post("/api/list-by-location", (req, res)=> {
